f1cd623e392323833d468121b612aebf4efc04c9
EtniAbigailEspinosaCasas <com.eespinosac24@alumnos.cecyteg.edu.mx>
2025-03-11 16:47:40 -0600

    Add files via upload

---
+++ 269.py
@@ -0,0 +1,12 @@
+var_a=6
+var_b=3
+suma=9
+resta=3
+multiplicasion=18
+division=2
+print("La suma de",var_a,"+",var_b,"es =",suma)
+print("La resta de",var_a,"-",var_b,"es =",resta)
+print("La multiplicasion de",var_a,"*",var_b,"es =",multiplicasion)
+print("La division de",var_a,"/",var_b,"es =",division)
+print("\n¡Eso es todo, amigos!")
+
